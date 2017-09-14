21fbc78b37feabf4038b5b4c17fb0cbf3a3ce6d2
Stefan Thon <smuemd@gmail.com>
2017-09-14 13:24:19 +0200

    Update Spike compiler configuration   => add Spike Records plugin to inject content as variables at compile time   => add Spike DataDir to read content from local markdown files   =>  compile markdown files to html   => don't minify html to maintain white-space for code highlighting also for production builds   => refactor production overwrites (in app.production.js)

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+const Records = require('spike-records')
+const DataSrc = require('spike-records-loaddir')
+const highlightjs = require('markdown-it-highlightjs')
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const env = process.env.SPIKE_ENV
 
+const locals = {}
+const posts = new DataSrc()
+
 module.exports = {
   devtool: 'source-map',
   ignore: [
     '**/layout.html',
-    '**/_*', '.*/**',
+    '**/_*',
+    '.*/**',
     '**/.*',
     'coverage/**',
     '.nyc_output/**',
@@ -18,9 +25,31 @@ module.exports = {
     'package-lock.json'
   ],
   reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
-    minify: env === 'production'
+    locals: ctx => {
+      locals.pageId = pageId(ctx)
+      locals.foo = 'bar'
+      return locals /* OLD { pageId: pageId(ctx), foo: 'bar' } */
+    },
+    markdownPlugins: [[highlightjs, { auto: false }]],
+    minify: false // env === 'production' // => does not work because code blocks in documentation!
   }),
+  plugins: [
+    new Records({
+      addDataTo: locals,
+      blog: {
+        data: posts.source('./data/articles'),
+        transform: (data) => (data.sort((a, b) => {
+          return (new Date(a.data.date).getTime() - new Date(b.data.date).getTime())
+        }).reverse()),
+        template: {
+          path: './views/_templates/_article.html',
+          output: function (post) {
+            return `articles/${post.data._slug}.html`
+          }
+        }
+      }
+    })
+  ],
   postcss: cssStandards({
     minify: env === 'production',
     warnForDuplicates: env !== 'production'
